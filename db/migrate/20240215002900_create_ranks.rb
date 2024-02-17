a1f35b9f92e2bfa47b81608c12a1379abe6b5ae4
watasho358 <t8mmmxc11y@yahoo.co.jp>
2024-02-17 19:26:07 +0900

    add: restrict コメント追記

--- db/migrate/20240215002900_create_ranks.rb
+++ db/migrate/20240215002900_create_ranks.rb
@@ -1,6 +1,7 @@
 class CreateRanks < ActiveRecord::Migration[6.0]
   def change
     create_table :ranks, comment: 'ゲーム内のランキング情報' do |t|
+      # resrtict 子テーブルのデータを持っている親テーブルのデータを削除したり、外部キーが設定されている項目の値を更新しようとするとエラーとして扱う
       t.references :user, null: false, index: { unique: true }, foreign_key: { on_delete: :restrict, on_update: :restrict }, comment: 'ユーザー'
 
       t.integer :rank, null: false, default: 0, index: true, comment: 'ユーザーの順位'
