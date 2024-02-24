0bfd89171dc80baedd78c0f8a5ca5132be5a9713
watasho358 <t8mmmxc11y@yahoo.co.jp>
2024-02-24 23:07:39 +0900

    add: メモリ消費量記録

--- app/models/chapter5/ranks_updater.rb
+++ app/models/chapter5/ranks_updater.rb
@@ -5,6 +5,7 @@ module Chapter5
       Rank.transaction do
         Rank.delete_all
         # ユーザーごとのスコア合計を降順に並べ替え、そこからランキング情報を再作成する
+        Development::UsedMemoryReport.instance.write('after Rank.delete_all')
         create_ranks
       end
     end
@@ -15,7 +16,11 @@ module Chapter5
         ranks << Rank.new(user_id: user.id, rank: rank, score: user.total_score)
       end
 
+      Development::UsedMemoryReport.instance.write('after RankOrderMaker.new.each_ranked_user')
+
       Rank.import ranks
+
+      Development::UsedMemoryReport.instance.write('after Rank.import')
     end
   end
 end
