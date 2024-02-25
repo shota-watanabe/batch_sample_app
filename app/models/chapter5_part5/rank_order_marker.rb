eb100dc0b957c7e545eef670d6ee56bcd2aa80b6
watasho358 <t8mmmxc11y@yahoo.co.jp>
2024-02-25 17:12:31 +0900

    add: Chapter5Part5::RankOrderMarker作成

---
+++ app/models/chapter5_part5/rank_order_marker.rb
@@ -0,0 +1,15 @@
+module Chapter5Part5
+  class RankOrderMaker
+    def each_ranked_user
+      # pluck(:score) => scoreのみを配列で取得
+      Rank
+        .distinct(:score)
+        .order(score: :desc)
+        .pluck(:score)
+        .each
+        .with_index(1) do |score, index|
+          yield(score, index)
+        end
+    end
+  end
+end
